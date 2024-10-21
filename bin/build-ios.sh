aaa721a486cb2d666cb838e20a89d6fd3288c639
Lukas Miklosko <lukas.miklosko@digitalsoftminds.eu>
2024-10-22 01:33:20 +0200

    iOS artifact

--- bin/build-ios.sh
+++ bin/build-ios.sh
@@ -18,3 +18,18 @@ mkdir -p lib/ios/
 libtool -static -o lib/ios/libtfcv-ios.a $(find build.ios.os/ -name '*.a')
 libtool -static -o lib/ios/libtfcv-simulator.a $(find build.ios.simulator/ -name '*.a')
 lipo -create lib/ios/libtfcv-ios.a lib/ios/libtfcv-simulator.a -output lib/ios/libtfcv.a
+
+# generate package
+mkdir -p build.ios/{lib,include}
+cp lib/ios/libtfcv-ios.a build.ios/lib/libtfcv.a
+
+TARGET_DIR=build.ios/include
+cp -r 3rdparty/opencv/modules/core/include/* $TARGET_DIR/
+cp -r 3rdparty/opencv/modules/imgproc/include/* $TARGET_DIR/
+cp -r 3rdparty/opencv/modules/imgcodecs/include/* $TARGET_DIR/
+cp -r 3rdparty/cxlog/include/cxlog $TARGET_DIR/.
+cp -r include/tfcv $TARGET_DIR/.
+
+cd build.ios
+zip -r release.zip .
+aws s3 cp release.zip s3://syngenta-of-pest-detection/releases/ios/1.3.2.zip
